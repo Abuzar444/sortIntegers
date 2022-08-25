afa14a35b7f17d63cf19f325888277b28ce03a58
= <=>
2022-08-25 22:01:53 +0500

    First Commit

---
+++ sortIntegers.js
@@ -0,0 +1,19 @@
+const sortByBits = function (arr) {
+    const count = (x) => {
+        const str = x.toString(2)
+        let num = 0
+        for (const c of str) {
+            if (c === '1') {
+                num += 1
+            }
+        }
+        return num
+    }
+    return arr.sort((a, b) => {
+        const countA = count(a)
+        const countB = count(b)
+        return countA === countB ? a - b : countA - countB
+    })
+}
+
+console.log(sortByBits([0, 1, 2, 3, 4, 5, 6, 7, 8]))
